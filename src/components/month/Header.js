a658dde977b5671161620d1d9d322a4526f76f06
hyunsu.lee <hyunsu.lee@navercorp.com>
2020-11-22 18:23:53 +0900

    해더 css 수정

--- src/components/month/Header.js
+++ src/components/month/Header.js
@@ -4,15 +4,17 @@ import className from 'classnames/bind'
 import {DAY, days} from 'constants/calendar'
 
 import styles from './Header.module.scss'
-className.bind(styles)
+const cx = className.bind(styles)
 
 const Header = ({start = DAY.MODAY}) => {
     return (
-        <ul>
-           {days[start].map((day, key) => (
-               <li key={key}>{day}</li>
-           ))}
-        </ul>
+        <div className={cx('header')}>
+            <ul >
+            {days[start].map((day, key) => (
+                <li key={key}>{day}</li>
+            ))}
+            </ul>
+        </div>
     );
 };
 
