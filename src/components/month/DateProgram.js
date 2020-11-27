8af1591f8a288753754987f30b976ee9ec88e1f6
hyunsu.lee <hyunsu.lee@navercorp.com>
2020-11-27 22:35:56 +0900

    #4 프로그램 수정

--- src/components/month/DateProgram.js
+++ src/components/month/DateProgram.js
@@ -17,10 +17,12 @@ const DateProgram = ({date}) => {
     const inputRef = useRef(null)
     let timeoutId = 0
 
+    // dispaly 상태를 토글한다.
     const handleToggleProramArea = () => {
         setHide(!hide)
     }
 
+    // 프로그램명 텍스트 변경을 처리한다.
     const handleProgramChange = ({currentTarget}) => {
         clearTimeout(timeoutId)
         timeoutId = setTimeout(() => {
@@ -28,12 +30,15 @@ const DateProgram = ({date}) => {
         }, 500)
     }
 
+    // 프로그래명 DB 업데이트를 한다
     const handleProramPatch = async (program) => {
         const {
             data: {success, result}
         } = await api.patch('/diary/program', {
             program,
-            todoDate: `${year}-${month}-${date}`
+            year,
+            month,
+            date
         })
 
         if (!success) {
@@ -44,10 +49,17 @@ const DateProgram = ({date}) => {
         setProgram(result.data.program)
     }
 
+    // input 태그 포커스 아웃이벤트를 처리한다.
     const handleProgramBlur = async ({currentTarget}) => {
         clearTimeout(timeoutId)
         await handleProramPatch(currentTarget.value)
         handleToggleProramArea()
+    }
+
+    const handleKeyUp = ({keyCode}) => {
+        if (keyCode === 13) {
+            inputRef.current.blur()
+        }
     }
 
     // input 태그 Show > input 태그에 auto focusing
@@ -73,6 +85,7 @@ const DateProgram = ({date}) => {
                     type="text"
                     onBlur={handleProgramBlur}
                     onChange={handleProgramChange}
+                    onKeyUp={handleKeyUp}
                 />
             </div>
         </div>
